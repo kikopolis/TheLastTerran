69315cb06158e2fbe21c815da38fd7405d8d62e4
kikod4mn <kristo.leas@gmail.com>
2023-02-02 07:09:23 +0200

    Added basic move and sprint in all directions

--- Assets/TestPlayMode/player_movement_test.cs
+++ Assets/TestPlayMode/player_movement_test.cs
@@ -6,6 +6,7 @@ using UnityEngine.TestTools;
 
 namespace TestPlayMode {
     public class player_movement_test {
+        private const float SLOW_TEST_WAIT = 1f;
         private float movementSpeed = 5f;
         private float sprintSpeed = 9f;
 
@@ -23,16 +24,90 @@ namespace TestPlayMode {
             yield return null;
             var inputManager = player.GetComponent<InputManager>();
             inputManager.move = new Vector2(0, 1);
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
             var newPosition = new Vector3(player.transform.position.x,
                                           player.transform.position.y,
                                           player.transform.position.z);
             yield return null;
             Assert.Less(newPosition.y, oldPosition.y);
             Assert.AreEqual(movementSpeed, controller.getCurrentSpeed());
-            Assert.AreEqual(newPosition.x, oldPosition.x);
-            Assert.AreEqual(newPosition.z, oldPosition.z);
-            SceneManager.UnloadSceneAsync("SampleScene");
+            Assert.True(Mathf.Approximately(newPosition.x, oldPosition.x));
+            Assert.Greater(newPosition.z, oldPosition.z);
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_move_backward() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.move = new Vector2(0, -1);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(movementSpeed, controller.getCurrentSpeed());
+            Assert.True(Mathf.Approximately(newPosition.x, oldPosition.x));
+            Assert.Less(newPosition.z, oldPosition.z);
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_move_left() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.move = new Vector2(-1, 0);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(movementSpeed, controller.getCurrentSpeed());
+            Assert.Less(newPosition.x, oldPosition.x);
+            Assert.True(Mathf.Approximately(newPosition.z, oldPosition.z));
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_move_right() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.move = new Vector2(1, 0);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(movementSpeed, controller.getCurrentSpeed());
+            Assert.Greater(newPosition.x, oldPosition.x);
+            Assert.True(Mathf.Approximately(newPosition.z, oldPosition.z));
         }
 
         [ UnityTest ]
@@ -50,16 +125,93 @@ namespace TestPlayMode {
             var inputManager = player.GetComponent<InputManager>();
             inputManager.sprint = true;
             inputManager.move = new Vector2(0, 1);
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
             var newPosition = new Vector3(player.transform.position.x,
                                           player.transform.position.y,
                                           player.transform.position.z);
             yield return null;
             Assert.Less(newPosition.y, oldPosition.y);
             Assert.AreEqual(sprintSpeed, controller.getCurrentSpeed());
-            Assert.AreEqual(newPosition.x, oldPosition.x);
-            Assert.AreEqual(newPosition.z, oldPosition.z);
-            SceneManager.UnloadSceneAsync("SampleScene");
+            Assert.True(Mathf.Approximately(newPosition.x, oldPosition.x));
+            Assert.Greater(newPosition.z, oldPosition.z);
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_sprint_backward() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.sprint = true;
+            inputManager.move = new Vector2(0, -1);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(sprintSpeed, controller.getCurrentSpeed());
+            Assert.True(Mathf.Approximately(newPosition.x, oldPosition.x));
+            Assert.Less(newPosition.z, oldPosition.z);
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_sprint_left() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.sprint = true;
+            inputManager.move = new Vector2(-1, 0);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(sprintSpeed, controller.getCurrentSpeed());
+            Assert.Less(newPosition.x, oldPosition.x);
+            Assert.True(Mathf.Approximately(newPosition.z, oldPosition.z));
+        }
+
+        [ UnityTest ]
+        public IEnumerator player_can_sprint_right() {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            yield return null;
+            var player = GameObject.FindGameObjectWithTag("Player");
+            var controller = player.GetComponent<PlayerController>();
+            // reset player rotation
+            player.transform.rotation = Quaternion.identity;
+            var oldPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            var inputManager = player.GetComponent<InputManager>();
+            inputManager.sprint = true;
+            inputManager.move = new Vector2(1, 0);
+            yield return new WaitForSeconds(SLOW_TEST_WAIT);
+            var newPosition = new Vector3(player.transform.position.x,
+                                          player.transform.position.y,
+                                          player.transform.position.z);
+            yield return null;
+            Assert.Less(newPosition.y, oldPosition.y);
+            Assert.AreEqual(sprintSpeed, controller.getCurrentSpeed());
+            Assert.Greater(newPosition.x, oldPosition.x);
+            Assert.True(Mathf.Approximately(newPosition.z, oldPosition.z));
         }
     }
 }
